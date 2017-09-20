e981d203a4546a4769d382aa5e4d6d4c57a05f79
Liam McArdle <mcardle.liam@gmail.com>
2017-09-19 19:30:22 -0700

    add phoenix based web app

---
+++ lib/gallows_web/views/hangman_view.ex
@@ -0,0 +1,21 @@
+defmodule GallowsWeb.HangmanView do
+  use GallowsWeb, :view
+
+  import Gallows.View.Helpers.GameStateHelper
+
+  def new_game_button(conn) do
+    button("New Game", to: hangman_path(conn, :create_game))
+  end
+
+  def game_over?(%{ game_state: game_state }) do
+    game_state in [ :won, :lost ]
+  end
+
+  def turn(left, target) when target >= left do
+    "opacity: 1"
+  end
+
+  def turn(left, target) do
+    "opacity: 0.1"
+  end
+end
